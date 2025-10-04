3c9dfc438f650511fe32dfa5470af677e28587fb
MSI <tulqinurinov005@gmail.com>
2025-10-04 18:56:35 +0500

    isValidSubsequence

---
+++ 42/ValidSubsequence.py
@@ -0,0 +1,25 @@
+"""
+Ikkita butun sonlardan iborat array beriladi. Ikkinchi array birinchining ketma-ket qismi ekanini aniqlovchi funksiya yozing.
+
+Ketma-ket qism — bu elementlari tartib bilan uchraydigan, lekin yonma-yon bo‘lishi shart bo‘lmagan qator.
+
+Masalan, [1, 3, 4] sonlari [1, 2, 3, 4] arrayining ketma-ket qismi hisoblanadi, [2, 4] ham shunday. Shuningdek, bitta son yoki arrayning o‘zi ham ketma-ket qism bo‘lishi mumkin.
+
+Misol uchun
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+"""
+
+
+def isValidSubsequence(array, sequence):
+    seq_index = 0
+
+    for value in array:
+        if seq_index == len(sequence):
+            break
+
+        if seq_index == array[value]:
+            seq_index += 1
+
+    return seq_index == len(sequence)
